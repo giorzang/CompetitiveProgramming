bc79594c0d4906991e7409b01760d9049b6d0006
Vũ Trường Giang <84860408+giorzang@users.noreply.github.com>
2025-09-27 21:44:57 +0000

    AC MovieFestival

---
+++ Usaco/Silver/MovieFestival.cpp
@@ -0,0 +1,83 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#ifndef Zang_LOCAL
+#define debug(x...) "giORZang"
+#else
+#define _NTH_ARG(_1, _2, _3, _4, _5, _6, N, ...) N
+#define _FE_0(_CALL, ...)
+#define _FE_1(_CALL, x) _CALL(x)
+#define _FE_2(_CALL, x, ...) _CALL(x) _FE_1(_CALL, __VA_ARGS__)
+#define _FE_3(_CALL, x, ...) _CALL(x) _FE_2(_CALL, __VA_ARGS__)
+#define _FE_4(_CALL, x, ...) _CALL(x) _FE_3(_CALL, __VA_ARGS__)
+#define _FE_5(_CALL, x, ...) _CALL(x) _FE_4(_CALL, __VA_ARGS__)
+#define FOR_EACH_MACRO(MACRO, ...) _NTH_ARG(dummy, ##__VA_ARGS__, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1, _FE_0) (MACRO, ##__VA_ARGS__)
+#define out(x) #x " = " << x << "; "
+#define debug(...) std::cerr << "Line " << __LINE__ << ": " FOR_EACH_MACRO(out, __VA_ARGS__) << std::endl
+#endif
+
+namespace {
+    #define sz(x) int(x.size())
+    #define bit(x, i) (((x) >> (i)) & 1)
+    #define all(a)  (a).begin(), (a).end()
+    #define rall(a) (a).rbegin(), (a).rend()
+    #define Gint ([&]() { ll x; return std::cin >> x, x; } ())
+
+    using ll = long long; 
+    using ld = long double;
+
+    template <class T1, class T2> 
+    bool chMax(T1 &a, T2 b) { return a < b && (a = b, true); }
+    template <class T1, class T2> 
+    bool chMin(T1 &a, T2 b) { return b < a && (a = b, true); }
+    template<class T1, class T2>
+    std::ostream &operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
+        return os << '{' << p.first << ", " << p.second << '}';
+    }
+    template <class T> using MaxHeap = std::priority_queue<T, std::vector<T>, std::less<T>>;
+    template <class T> using MinHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+    template <class T, class = std::decay_t<decltype(*begin(std::declval<T>()))>, class = std::enable_if_t<!std::is_same<T, std::string>::value>>
+    std::ostream &operator<<(std::ostream &os, const T &c) {
+        os << '[';
+        for (auto it = c.begin(); it != c.end(); ++it) {
+            os << &", "[2 * (it == c.begin())] << *it;
+        }
+        return os << ']';
+    }
+
+    const ld eps = 1e-6, pi = acos(-1);
+    const ll mod = 1e9 + 7, e30 = 1ll << 30, e60 = 1ll << 60;
+}
+
+using namespace std;
+
+signed main() {
+    function<void(string)> fileio = [&](string Task) -> void {
+        if (fopen((Task + ".inp").c_str(), "r")) {
+            freopen((Task + ".inp").c_str(), "r", stdin);
+            freopen((Task + ".out").c_str(), "w", stdout); 
+        }
+    }; fileio("");
+    cin.tie(nullptr) -> sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].second >> a[i].first;
+    }
+    sort(all(a));
+
+    int cur = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i].second >= cur) {
+            ++ans;
+            cur = a[i].first;
+        }
+    }
+    cout << ans;
+
+    fprintf(stderr, "\n=====\nUsed: %.f ms\n", 1000.0 * clock() / CLOCKS_PER_SEC);
+    return (0 ^ 0);
+}
